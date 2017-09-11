6a151614f09e400e07d5db242c666f07ae1dd228
Andrey Makimov <onixbed@gmail.com>
2017-09-11 09:19:34 +0600

    [*] fix опечатка в методе title2Permlink

--- src/main/java/ru/maksimov/andrey/golos4j/util/Util.java
+++ src/main/java/ru/maksimov/andrey/golos4j/util/Util.java
@@ -332,7 +332,7 @@ public class Util {
 		if (StringUtils.isBlank(title)) {
 			throw new BusinessException("Unable conver title tp permlink");
 		}
-		String[] abcCyr = { "a", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р",
+		String[] abcCyr = { "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р",
 				"с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", " ", ".", "!" };
 		String[] abcLat = { "a", "b", "v", "g", "d", "e", "jo", "zh", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r",
 				"s", "t", "u", "f", "h", "ts", "ch", "sh", "sch", "b", "", "", "e", "ju", "ja", "-", "-", "-" };
